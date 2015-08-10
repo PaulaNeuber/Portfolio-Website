427e8a476f48b9a5f2c8673ecc58e60c14990ae5
PaulaNeuber <p.neuber@zeppelin-university.net>
2015-08-10 12:33:00 +0100

    13.1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+ $("button").on("click"), function(){
+    //now we're going to work in here
+    //all the rest of our code goes up here
+    console.log("clicked");
+    return false;
+    // no code here!
+}); 
