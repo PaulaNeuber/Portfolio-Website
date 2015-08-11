69a36d4d0bca3ddb67f1be435f142ebe513c547a
PaulaNeuber <p.neuber@zeppelin-university.net>
2015-08-11 14:37:12 +0200

    16.1.3.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){ 
   $("#test").on("click", function(){
-    console.log("clicked");
-    return false;
+    var comment= $(".message-box").val();
+    console.log(comment)
   });
 
   $(".message-box").on("keyup", function(){
     console.log("keyup happened");
+    var nameName=3;
+    var name="string";
+    var charCount = $(".message-box").val().length
+    console.log(charCount)
   });
 
-  var comment= $(".message-box").val();
-    console.log(comment)
-
-  var nameName=3;
-  var name="string";
-  var charCount = $(".message-box").val().length
-    console.log(charCount)
 });
 
