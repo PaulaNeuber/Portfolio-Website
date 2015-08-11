ad8a7d9ab7ca57eafff0435ab2ea281d185da4d5
PaulaNeuber <p.neuber@zeppelin-university.net>
2015-08-11 14:14:08 +0200

    16.1.2

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
     console.log("clicked");
     return false;
   });
+
   $(".message-box").on("keyup", function(){
     console.log("keyup happened");
   });
+
+  var comment= $(".message-box").val();
+    console.log(comment)
+
   var nameName=3;
   var name="string";
   var charCount = $(".message-box").val().length
-  console.log(charCount)
+    console.log(charCount)
 });
 
