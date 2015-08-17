8259c74696a6147939b01c9fa214b62b787c0532
PaulaNeuber <p.neuber@zeppelin-university.net>
2015-08-17 21:04:00 +0200

    19

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
+var myKitties = [ 
+{ title: "Malteser Hilfsdienst e.V.", pic: "img/malteser2.jpg" }, 
+{ title: "MWS Industrie- holding GmbH", pic: "img/mws.png" }, 
+{ title: "Youth for Understanding e.V.", pic: "yfu.png" }, 
+{ title: "Motorradland GmbH", pic: "img/motorradland.png" }
+];
 $(document).ready(function(){ 
-  var rows=$(".my-row");
-  console.log(rows);
-for (var i=0; i<rows.length; ++i) {
-  if (i%2===0) {
-    $(rows[i]).css("background-color","blue");
-  };
-};
+//   var rows=$(".my-row");
+//   console.log(rows);
+// for (var i=0; i<rows.length; ++i) {
+//   if (i%2===0) {
+//     $(rows[i]).css("background-color","blue");
+//   };
+// };
+  for(var i=0; i<myKitties.length; ++i) {
+        $( "#" + i ).css("background-image", "url(" + myKitties[i].pic + ")" );
+  }; 
 
-  $(".message-box").on("keyup", function(){
+$(".image").mouseenter( function() {
+      console.log(myKitties[this.id].title);
+      $(this).html("<p class='info'><span class='proj-title'></span> " + myKitties[this.id].title + "</p>");
+}).mouseleave( function() {
+      $("p.info").html("");
+});
+
+
+  $(".message-box").on("keyup", function() {
     console.log("keyup happened");
     var nameName=3;
     var name="string";
@@ -20,6 +37,9 @@ for (var i=0; i<rows.length; ++i) {
           $("#char-count").css("color","black");
         };
   });
+
+ 
+ 
 
 });
 
